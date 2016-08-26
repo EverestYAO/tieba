8d591154626a661ca6b13df2752b452e337d726d
EverestYAO <zaojue405@aliyun.com>
2016-08-26 15:53:45 +0800

    auto

--- tieba.py
+++ tieba.py
@@ -3,8 +3,8 @@ import urllib
 import urllib2
 import re
 import sys
-def open_url(url2):
-	req = urllib2.Request(url2)
+def open_url(url):
+	req = urllib2.Request(url)
 	user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit'
 	headers = {'User-Agent' : user_agent}
 	try:page = urllib2.urlopen(req)
@@ -14,6 +14,18 @@ def open_url(url2):
 	html = page.read().decode('utf-8')
 	return html
 	
+def get_pages(url):
+	req = urllib2.Request(url)
+	user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit'
+	headers = {'User-Agent' : user_agent}
+	try:page = urllib2.urlopen(req)
+	except urllib2.URLError,e:
+		print e.reason
+	html = page.read().decode('utf-8')
+	t = re.compile(r'cur_page":\d,"total_page":(\d*)')
+	pa = t.findall(html)
+	return pa
+	
 def get_img(html):
 	p = re.compile(r'src="([^"]+\.jpg)"')
 	imglist = p.findall(html)
@@ -22,16 +34,22 @@ def get_img(html):
 		filename = each[-8:]
 		try:
 			urllib.urlretrieve(each,filename,None)
-			print "downloading" + str(i) + "个新垣结衣"
+			print "downloading" + str(i) + u" 张小黄图"
 		except:
 			print "Unexpected error:", sys.exc_info()[0]
 		i+= 1
+		
 def start():
-	for j in range(1,pages+1):
-		url2 = url + str(j)
-		get_img(open_url(url2))
-		print '已经爬取了第 ' + str(j) + ' 页'
+	a = get_pages(url)
+	for x in a:
+		for j in range(1,int(x)+1):
+			url2 = url[:-1] + str(j) 
+			get_img(open_url(url2))
+			print '已经爬取了第 ' + str(j) + ' 页'
+		
+
 if __name__=='__main__':
-	url = raw_input('输入网址，把pn=后面的数字去掉')
-	pages = int(raw_input('该帖子有多少页？'))
+	print "百度贴吧可以直接使用"
+	url = raw_input('输入网址把: ')
+	print url[:-6]
 	start()
